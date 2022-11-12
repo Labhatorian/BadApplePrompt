e9d9d23bee81efef36a9b08d99f36d50631f7ea2
Harris <16213031+Labhatorian@users.noreply.github.com>
2022-11-12 15:46:19 +0100

    Get video information now (ffprobe) General improvements

--- BadAppleCMD/Program.cs
+++ BadAppleCMD/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Drawing;
-using System.Globalization;
 using System.Media;
 using System.Text;
 using System.Timers;
@@ -8,24 +7,21 @@ using Timer = System.Timers.Timer;
 
 namespace BadAppleCMD
 {
-    internal class Program
-    {
+    public class Program
+    {   //Program
+        private static string? strWorkPath = "";
+
+        //Videoplayer
         private static int _framecounter = 0;
         private static int _totalframecounter = 1;
         private static string _FPS = "FPS: 0";
         private static Boolean _Desync = false;
-        private static SoundPlayer audio;
-        private static string strWorkPath = "";
-
-        //For loading bars
-        private static string totalduration;
-        private static string currentdurationframe;
-        private static Boolean loadingFinished;
+        private static SoundPlayer? audio;
 
         //Video information
-        private static int _VideoWidth;
-        private static int _VideoHeight;
-        private static int _VideoFrameRate;
+        public static int VideoWidth { get; set; }
+        public static int VideoHeight { get; set; }
+        public static int VideoFrameRate { get; set; }
 
         static void Main(string[] args)
         {
@@ -47,7 +43,7 @@ namespace BadAppleCMD
             else
             {
                 //todo clear this out for release
-                WriteScreen(ConsoleColor.DarkRed, "No file has been provided", "Drag and drop a video on the executable. The program will close in 5 seconds.");
+                Screens.WriteScreen(ConsoleColor.DarkRed, "No file has been provided", "Drag and drop a video on the executable. The program will close in 5 seconds.");
                 //Thread.Sleep(5000);
                 //Environment.Exit(0);
             }
@@ -59,39 +55,29 @@ namespace BadAppleCMD
             DirectoryInfo di = Directory.CreateDirectory(strWorkPath + "/temp");
             di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
 
-            //Save frames and get audio
-            //todo convert to use method
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.Clear();
-            string MainString = "Now Playing:";
-            Console.SetCursorPosition((Console.WindowWidth - MainString.Length) / 2, Console.WindowHeight / 2 - 3);
-            Console.WriteLine(MainString);
-
-            Console.SetCursorPosition((Console.WindowWidth - path.Length) / 2, Console.WindowHeight / 2);
-            Console.WriteLine(path);
+            Screens.WriteScreen(ConsoleColor.DarkBlue, "Now Playing", path);
             Thread.Sleep(2000);
 
             //Get information about video
-            //todo loading bar but instead with information WOW!
             string parameter = "-v error -select_streams v:0 -show_entries stream=width,height,avg_frame_rate -of default=nw=1 " + path;
             Task task = Task.Run(() => { Execute(".\\ffprobe.exe", parameter, true); });
-            LoadingBar("Getting information about the video...");
+            Screens.InformationOrLoadingBar("Getting information about the video...", true);
+            Thread.Sleep(2000);
 
-            //TODO Add nifty loading bar instead of outputting it. allow for verbose as arg/option
+            //TODO Allow verbose loading bars
             parameter = "-i " + path + " " + strWorkPath + "/temp/%04d.png";
             parameter = parameter.Replace("\\", "/");
             Console.CursorVisible = false;
             task = Task.Run(() => { Execute(".\\ffmpeg.exe", parameter, true); });
-            LoadingBar("Getting every frame from the video...");
+            Screens.InformationOrLoadingBar("Getting every frame from the video...", false);
 
             parameter = "-i " + path + " " + strWorkPath + "/temp/audio.wav";
             parameter = parameter.Replace("\\", "/");
 
-            //todo do same as getting video file
             if (!File.Exists(strWorkPath + "/temp/audio.wav"))
             {
                 task = Task.Run(() => { Execute(".\\ffmpeg.exe", parameter, false); });
-                LoadingBar("Getting audio from the video...");
+                Screens.InformationOrLoadingBar("Getting audio from the video...", false);
             }
 
             Console.WriteLine("Finished");
@@ -104,6 +90,7 @@ namespace BadAppleCMD
             //todo use video resolution to get these values. Figure out max for 1920x1080?
             Console.SetWindowSize(120, 46 + 1);
             Console.SetBufferSize(120, 46 + 1);
+
             //TODO Disable window resizing and window maximising
 
             //FPS counter
@@ -115,19 +102,18 @@ namespace BadAppleCMD
             audio.Play();
             timer.Start();
 
-            //todo get fps from video to use as values here
             while (_totalframecounter < fCount)
             {
-                if (_framecounter != _VideoFrameRate)
+                if (_framecounter != VideoFrameRate)
                 {
                     //todo calculate how many 0s are needed
-                    using (Bitmap image = new Bitmap(strWorkPath + $"\\temp\\{_totalframecounter:0000}.png"))
+                    using (Bitmap image = new(strWorkPath + $"\\temp\\{_totalframecounter:0000}.png"))
                     {
                         Console.Write(ConvertToAscii(image));
                     }
                     Console.WriteLine(_FPS);
                     File.Delete(strWorkPath + $"\\temp\\{_totalframecounter:0000}.png");
-                    Thread.Sleep(_VideoFrameRate/2);
+                    Thread.Sleep(VideoFrameRate / 2);
                     Console.SetCursorPosition(0, 0);
                     _framecounter++;
                     _totalframecounter++;
@@ -136,76 +122,10 @@ namespace BadAppleCMD
             Environment.Exit(0);
         }
 
-        private static void WriteScreen(ConsoleColor BackgroundColour, string MainString, string SecondString)
-        {
-            Console.BackgroundColor = BackgroundColour;
-            Console.Clear();
-            Console.SetCursorPosition((Console.WindowWidth - MainString.Length) / 2, Console.WindowHeight / 2 - 3);
-            Console.WriteLine(MainString);
-            Console.SetCursorPosition((Console.WindowWidth - SecondString.Length) / 2, Console.WindowHeight / 2);
-            Console.WriteLine(SecondString);
-        }
-
-        private static void LoadingBar(string MainString)
-        {
-            //todo add additional option to display a third line
-            //todo Use WriteScreen() to save code
-            Console.Clear();
-            while (!loadingFinished)
-            {
-                Console.SetCursorPosition((Console.WindowWidth - MainString.Length) / 2, Console.WindowHeight / 2 - 3);
-                Console.WriteLine(MainString);
-                int totaldurationseconds = 1;
-                int currentdurationseconds = 0;
-
-                //convert to percentage
-                if (totalduration is not null && currentdurationframe is not null)
-                {
-                    try
-                    {
-                        DateTime totaldurationTime = DateTime.ParseExact(totalduration, "HH:mm:ss.ff",
-                                        CultureInfo.InvariantCulture);
-                        totaldurationseconds = totaldurationTime.Hour * 60 * 60 + totaldurationTime.Minute * 60 + totaldurationTime.Second + totaldurationTime.Millisecond / 100;
-
-                        DateTime currentdurationTime = DateTime.ParseExact(currentdurationframe, "HH:mm:ss.ff",
-                                               CultureInfo.InvariantCulture);
-                        currentdurationseconds = currentdurationTime.Hour * 60 * 60 + currentdurationTime.Minute * 60 + currentdurationTime.Second + currentdurationTime.Millisecond / 100;
-                    }
-                    catch (System.FormatException)
-                    {
-                        //ffmpeg hasnt started properly yet, ignore
-                    }
-                }
-
-                StringBuilder loadingbar = new("[");
-                int totalBars = (int)Math.Ceiling((double)(((double)currentdurationseconds / (double)totaldurationseconds * (double)100) / (double)5));
-
-                for (int i = 1; i <= 20; i++)
-                {
-                    if (totalBars > 0)
-                    {
-                        loadingbar.Append("█");
-                        totalBars--;
-                    }
-                    else
-                    {
-                        loadingbar.Append(" ");
-                    }
-                    if (i != 20)
-                    {
-                        loadingbar.Append("|");
-                    }
-                }
-                loadingbar.Append("]");
-                Console.SetCursorPosition((Console.WindowWidth - loadingbar.ToString().Length) / 2, Console.WindowHeight / 2);
-                Console.WriteLine(loadingbar.ToString());
-            }
-        }
-
         private static void Execute(string exePath, string parameters, Boolean getinformation)
         {
             string result = String.Empty;
-            loadingFinished = false;
+            Screens.LoadingFinished = false;
 
             using Process p = new();
             p.StartInfo.UseShellExecute = false;
@@ -215,6 +135,7 @@ namespace BadAppleCMD
             p.StartInfo.FileName = exePath;
             p.StartInfo.Arguments = parameters;
 
+            //For ffprobe
             p.OutputDataReceived += new DataReceivedEventHandler((s, e) =>
             {
                 ///Console.WriteLine(e.Data);
@@ -224,25 +145,26 @@ namespace BadAppleCMD
                     {
                         if (e.Data.Contains("width="))
                         {
-                            _VideoWidth = int.Parse(e.Data.Substring(e.Data.LastIndexOf("width=") + 6));
+                            VideoWidth = int.Parse(e.Data[(e.Data.LastIndexOf("width=") + 6)..]);
                         }
 
                         if (e.Data.Contains("height="))
                         {
-                            _VideoHeight = int.Parse(e.Data.Substring(e.Data.LastIndexOf("height=") + 7));
+                            VideoHeight = int.Parse(e.Data[(e.Data.LastIndexOf("height=") + 7)..]);
                         }
 
                         if (e.Data.Contains("avg_frame_rate="))
                         {
-                            string frameratefraction = e.Data.Substring(e.Data.LastIndexOf("avg_frame_rate=") + 15);
+                            string frameratefraction = e.Data[(e.Data.LastIndexOf("avg_frame_rate=") + 15)..];
                             int valueOne = int.Parse(frameratefraction.Split('/')[0]);
-                            int valueTwo = int.Parse(frameratefraction.Substring(frameratefraction.LastIndexOf('/') + 1));
-                            _VideoFrameRate = valueOne / valueTwo;
+                            int valueTwo = int.Parse(frameratefraction[(frameratefraction.LastIndexOf('/') + 1)..]);
+                            VideoFrameRate = valueOne / valueTwo;
                         }
                     }
                 }
             });
 
+            //For ffmpeg
             p.ErrorDataReceived += new DataReceivedEventHandler((s, e) =>
             {
                 ///Console.WriteLine(e.Data);
@@ -252,11 +174,11 @@ namespace BadAppleCMD
                     {
                         if (e.Data.Contains("Duration:") && getinformation)
                         {
-                            totalduration = e.Data.Substring(e.Data.LastIndexOf("Duration:") + 10, e.Data.LastIndexOf("Duration:") + 9);
+                            Screens.TotalDuration = e.Data.Substring(e.Data.LastIndexOf("Duration:") + 10, e.Data.LastIndexOf("Duration:") + 9);
                         }
                         if (e.Data.Contains("time="))
                         {
-                            currentdurationframe = e.Data.Substring(e.Data.LastIndexOf("time=") + 5, e.Data.LastIndexOf("time=:") + 12);
+                            Screens.CurrentDuration = e.Data.Substring(e.Data.LastIndexOf("time=") + 5, e.Data.LastIndexOf("time=:") + 12);
                         }
                     }
 
@@ -267,7 +189,7 @@ namespace BadAppleCMD
             p.BeginOutputReadLine();
             p.BeginErrorReadLine();
             p.WaitForExit();
-            loadingFinished = true;
+            Screens.LoadingFinished = true;
         }
 
         private static string ConvertToAscii(Bitmap image)
@@ -304,7 +226,7 @@ namespace BadAppleCMD
         private static void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
             _FPS = $"FPS: {_framecounter}";
-            if (_framecounter < _VideoFrameRate && !_Desync)
+            if (_framecounter < VideoFrameRate && !_Desync)
             {
                 _Desync = true;
             }
@@ -318,21 +240,16 @@ namespace BadAppleCMD
         static void ExitApplication(object sender, EventArgs e)
         {
             //todo test every case
-            //todo different message on program error
-            WriteScreen(ConsoleColor.DarkBlue, "Thank you for playing!", "Cleaning up and exiting...");
+            //todo different message on program error?
+            Screens.WriteScreen(ConsoleColor.DarkBlue, "Thank you!", "Cleaning up and exiting...");
 
-            //todo check for null
-            if (audio is not null)
-            {
-                audio.Stop();
-            }
-
+            audio?.Stop();
             _totalframecounter = 999999999;
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
             //Delete temp folder
-            if(Directory.Exists(strWorkPath + "/temp"))
+            if (Directory.Exists(strWorkPath + "/temp"))
             {
                 Directory.Delete(strWorkPath + "/temp", true);
             }
---
+++ BadAppleCMD/Screens.cs
@@ -0,0 +1,102 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace BadAppleCMD
+{
+    public static class Screens
+    {
+        public static string? TotalDuration { get; set; }
+        public static string? CurrentDuration { get; set; }
+        public static Boolean LoadingFinished { get; set; }
+
+        public static void WriteScreen(ConsoleColor BackgroundColour, string MainString, string SecondString)
+        {
+            Console.BackgroundColor = BackgroundColour;
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - MainString.Length) / 2, Console.WindowHeight / 2 - 3);
+            Console.WriteLine(MainString);
+            Console.SetCursorPosition((Console.WindowWidth - SecondString.Length) / 2, Console.WindowHeight / 2);
+            Console.WriteLine(SecondString);
+        }
+
+        public static void InformationOrLoadingBar(string MainString, Boolean Information)
+        {
+            //todo add additional option to display a third line
+            //todo Use WriteScreen() to save code
+            Console.Clear();
+            while (!LoadingFinished)
+            {
+                Console.SetCursorPosition((Console.WindowWidth - MainString.Length) / 2, Console.WindowHeight / 2 - 3);
+                Console.WriteLine(MainString);
+
+
+                //convert to percentage
+                StringBuilder loadingbar = new("[");
+
+                if (!Information)
+                {
+                    LoadingBar(loadingbar);
+                }
+                else
+                {
+                    InformationBar(loadingbar);
+                }
+
+                loadingbar.Append(']');
+                Console.SetCursorPosition((Console.WindowWidth - loadingbar.ToString().Length) / 2, Console.WindowHeight / 2);
+                Console.WriteLine(loadingbar.ToString());
+            }
+        }
+
+        private static void LoadingBar(StringBuilder loadingbar)
+        {
+            int totaldurationseconds = 1;
+            int currentdurationseconds = 0;
+
+            if (TotalDuration is not null && CurrentDuration is not null)
+            {
+                try
+                {
+                    DateTime totaldurationTime = DateTime.ParseExact(TotalDuration, "HH:mm:ss.ff",
+                                    CultureInfo.InvariantCulture);
+                    totaldurationseconds = totaldurationTime.Hour * 60 * 60 + totaldurationTime.Minute * 60 + totaldurationTime.Second + totaldurationTime.Millisecond / 100;
+
+                    DateTime currentdurationTime = DateTime.ParseExact(CurrentDuration, "HH:mm:ss.ff",
+                                           CultureInfo.InvariantCulture);
+                    currentdurationseconds = currentdurationTime.Hour * 60 * 60 + currentdurationTime.Minute * 60 + currentdurationTime.Second + currentdurationTime.Millisecond / 100;
+                }
+                catch (System.FormatException)
+                {
+                    //ffmpeg hasnt started properly yet, ignore
+                }
+            }
+
+
+            int totalBars = (int)Math.Ceiling((double)(((double)currentdurationseconds / (double)totaldurationseconds * (double)100) / (double)5));
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (totalBars > 0)
+                {
+                    loadingbar.Append('█');
+                    totalBars--;
+                }
+                else
+                {
+                    loadingbar.Append(' ');
+                }
+                if (i != 20)
+                {
+                    loadingbar.Append('|');
+                }
+            }
+        }
+
+        private static void InformationBar(StringBuilder loadingbar)
+        {
+            loadingbar.Append($"Resolution= {Program.VideoWidth}x{Program.VideoHeight}");
+            loadingbar.Append(" | ");
+            loadingbar.Append($"Framerate= {Program.VideoFrameRate}");
+        }
+    }
+}
